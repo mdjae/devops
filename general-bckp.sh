15de4ba07999ea277270423ae15e35e8f929d29a
DJAE Mamode <infomamode@gmail.com>
2016-05-12 17:36:33 +0400

    Update general-bckp.sh

--- general-bckp.sh
+++ general-bckp.sh
@@ -14,7 +14,10 @@ DATE='data +%d%m%y'
 REMOTESRV[0]=xx.xxx.xxx.xxx:bckp:/var/www
 #REMOTESRV[1]=xx.xxx.xxx.xxx:bckp:/var/www
 #REMOTESRV[2]=xx.xxx.xxx.xxx:bckp:/var/www
+for elt in ${REMOTESRV[@]}
+do  
+  IFS=':' read -a SRV <<< "$elt"
+  # Execute backup command on remote server
+  ssh ${SRV[0]}@$${SRV[1]} ${SRV[2]} 
 
-IFS=':' read -a SRV <<< "${REMOTESRV[$i]}"
-ssh ${SRV[0]}@$${SRV[1]} ${SRV[2]} 
-
+done

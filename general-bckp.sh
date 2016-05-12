167313e1841841cdaa9e91320300c333d911be70
DJAE Mamode <infomamode@gmail.com>
2016-05-12 17:33:08 +0400

    Update general-bckp.sh

--- general-bckp.sh
+++ general-bckp.sh
@@ -1,1 +1,20 @@
 #!/bin/sh
+# -----------------------------------------------------
+# General purpose backup script
+# author : mdjae
+# -----------------------------------------------------
+
+# Set start time 
+SECONDS=0
+DATE='data +%d%m%y'
+
+# Remote server config 
+# Add server with REMOTESRV[n]=IP|USER|PATH
+
+REMOTESRV[0]=xx.xxx.xxx.xxx:bckp:/var/www
+#REMOTESRV[1]=xx.xxx.xxx.xxx:bckp:/var/www
+#REMOTESRV[2]=xx.xxx.xxx.xxx:bckp:/var/www
+
+IFS=':' read -a SRV <<< "${REMOTESRV[$i]}"
+ssh ${SRV[0]}@$${SRV[1]} ${SRV[2]} 
+
